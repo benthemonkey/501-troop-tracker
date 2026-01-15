ed84ea751ad21970b5e8163172973eb7637de0e8
Ben Rothman <bensrothman@gmail.com>
2026-01-15 11:43:45 -0600

    tweak css

--- src/index.php
+++ src/index.php
@@ -56,7 +56,7 @@ echo '
 
 echo '
 	<!-- Style Sheets -->
-	<link href="css/main.css?v=2" rel="stylesheet" />
+	<link href="css/main.css?v=3" rel="stylesheet" />
 	<link rel="stylesheet" href="script/lib/jquery-ui.min.css">
 	<link rel="stylesheet" href="script/lib/jquery-ui-timepicker-addon.css">
 	<link href="css/dropzone.min.css" type="text/css" rel="stylesheet" />
