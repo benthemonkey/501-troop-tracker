345754f7bb1347a9c4d9b634972086a45eb43f22
Ben Rothman <bensrothman@gmail.com>
2026-02-20 16:47:22 -0600

    hide extra squad in edit event

--- src/index.php
+++ src/index.php
@@ -3708,7 +3708,7 @@ if (isset($_GET['action']) && $_GET['action'] == "commandstaff") {
 						<select name="squadm" id="squadm">
 							<option value="null" SELECTED>Please choose an option...</option>
 							' . squadSelectList(false) . '
-							<option value="0">' . garrison . '</option>
+							<!-- <option value="0">' . garrison . '</option> -->
 						</select>		
 
 						<p>Date/Time Start:</p>
